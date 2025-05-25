89a9e051d33f8151ddd0fe87393b835caa79ee2e
ardiansa22 <ferianardiansa22@gmail.com>
2025-05-25 22:52:36 +0700

    logo

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,6 +1,15 @@
 <nav class="mobile-navbar d-md-none">
-    <a href="{{route('home')}}" wire:navigate class="nav-item"><i class="fas fa-home"></i><span>Home</span></a>
-    <a href="" class="nav-item"><i class="fas fa-map-marker-alt"></i><span>Explore</span></a>
-    <a href="{{route('my-books')}}" wire:navigate class="nav-item"><i class="fas fa-calendar-alt"></i><span>MyBook</span></a>
-    <a href="#" class="nav-item"><i class="fas fa-user"></i><span>Profil</span></a>
-  </nav>
+    <a href="{{ route('home') }}" wire:navigate class="nav-item">
+        <i class="fas fa-home"></i><span>Home</span>
+    </a>
+    
+    <a href="" class="nav-item">
+        <i class="fas fa-map-marker-alt"></i><span>Explore</span>
+    </a>
+
+    @auth
+    <a href="{{ route('my-books') }}" wire:navigate class="nav-item">
+        <i class="fas fa-calendar-alt"></i><span>MyBook</span>
+    </a>
+    @endauth
+</nav>
