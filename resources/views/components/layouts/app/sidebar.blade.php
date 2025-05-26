722a43356e8b2b3876bf0994ebfaeecf36bb8aa1
ardiansa22 <ferianardiansa22@gmail.com>
2025-05-26 10:51:17 +0700

    logo

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -9,8 +9,8 @@
     <flux:sidebar sticky stashable class="bg-zinc-50 dark:bg-zinc-900 border-r rtl:border-r-0 rtl:border-l border-zinc-200 dark:border-zinc-700">
         <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
-        <flux:brand href="#" logo="{{ asset('assets/images/logo.png') }}" name="CereBook." class="px-2 dark:hidden" />
-        <flux:brand href="#" logo="{{ asset('assets/images/logo.png') }}" name="CereBook." class="px-2 hidden dark:flex" />
+        <flux:brand href="#" logo="https://matapangandaran.my.id/images/logo.png" name="CereBook." class="px-2 dark:hidden" />
+        <flux:brand href="#" logo="https://matapangandaran.my.id/images/logo.png" name="CereBook." class="px-2 hidden dark:flex" />
 
         <flux:input as="button" variant="filled" placeholder="Search..." icon="magnifying-glass" />
 
