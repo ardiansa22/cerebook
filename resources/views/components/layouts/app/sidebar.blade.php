5452ad963a35a62f2099c67f530fc0b1fd1b48f8
ardiansa22 <ferianardiansa22@gmail.com>
2025-05-26 10:49:17 +0700

    logo

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -9,18 +9,23 @@
     <flux:sidebar sticky stashable class="bg-zinc-50 dark:bg-zinc-900 border-r rtl:border-r-0 rtl:border-l border-zinc-200 dark:border-zinc-700">
         <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
-        <flux:brand href="#" logo="https://fluxui.dev/img/demo/logo.png" name="Acme Inc." class="px-2 dark:hidden" />
-        <flux:brand href="#" logo="https://fluxui.dev/img/demo/dark-mode-logo.png" name="Acme Inc." class="px-2 hidden dark:flex" />
+        <flux:brand href="#" logo="{{ asset('assets/images/logo.png') }}" name="CereBook." class="px-2 dark:hidden" />
+        <flux:brand href="#" logo="{{ asset('assets/images/logo.png') }}" name="CereBook." class="px-2 hidden dark:flex" />
 
         <flux:input as="button" variant="filled" placeholder="Search..." icon="magnifying-glass" />
 
         <flux:navlist variant="outline">
             <flux:navlist.item icon="home" href="/dashboard" wire:navigate>Home</flux:navlist.item>
-            <flux:navlist.item icon="inbox" badge="12" href="/book" wire:navigate>Books</flux:navlist.item>
-            <flux:navlist.item icon="document-text" href="/categories" wire:navigate>Categori</flux:navlist.item>
-            <flux:navlist.item icon="calendar" href="/genre" wire:navigate>Genre</flux:navlist.item>
-            <flux:navlist.item icon="calendar" href="/product-category" wire:navigate>Category Product</flux:navlist.item>
-            <flux:navlist.item icon="calendar" href="/products" wire:navigate>Product</flux:navlist.item>
+
+             <flux:navlist.group expandable heading="Book" class="hidden lg:grid">
+                <flux:navlist.item icon="inbox" badge="12" href="/book" wire:navigate>Books</flux:navlist.item>
+                <flux:navlist.item icon="document-text" href="/categories" wire:navigate>Categori</flux:navlist.item>
+                <flux:navlist.item icon="calendar" href="/genre" wire:navigate>Genre</flux:navlist.item>
+            </flux:navlist.group>
+             <flux:navlist.group expandable heading="Non-Book" class="hidden lg:grid">
+               <flux:navlist.item icon="calendar" href="/product-category" wire:navigate>Category Product</flux:navlist.item>
+                <flux:navlist.item icon="calendar" href="/products" wire:navigate>Product</flux:navlist.item>
+            </flux:navlist.group>
         </flux:navlist>
 
         <flux:spacer />
